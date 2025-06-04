e13deaea2679a6de9ff59af5eef3e8fd8e065267
Leo Laaksonen <leo.laaksonen@hotmail.com>
2025-06-04 16:58:42 +0300

    one message tester

---
+++ onemessage.php
@@ -0,0 +1,22 @@
+<?php
+
+function main(): int
+{
+    $sock = socket_create(AF_INET, SOCK_STREAM, 0);
+    $conn = socket_connect($sock, "127.0.0.1", 1234);
+    if (!$conn)
+	    return 1;
+    $x = "POST test.txt HTTP/1.1\r\nContent-Length: 40\r\n\r\nI am the content of test.txt\r\n";
+    socket_write($sock,$x, strlen($x));
+    sleep(1);
+
+    $response = '';
+    $out = socket_read($sock, 2048);
+    
+    echo "Server response:\n";
+    echo $out . "\n";
+    return 0;
+}
+
+exit(main());
+
