ab456b5b85f9840288e39f270a18ef73811b1e44
Aina Albrecht <aalbrech@c2r1p2.hive.fi>
2025-08-01 12:14:56 +0300

    cgi-php works

--- src/request/cgi.cpp
+++ src/request/cgi.cpp
@@ -6,7 +6,7 @@
 /*   By: aalbrech <aalbrech@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/24 12:25:17 by aalbrech          #+#    #+#             */
-/*   Updated: 2025/07/31 18:47:19 by aalbrech         ###   ########.fr       */
+/*   Updated: 2025/08/01 12:03:04 by aalbrech         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,6 +24,7 @@ std::vector<char *>HttpRequest::setupCgiEnv(ServerConfig config, std::string pat
 	envVariables.push_back("SERVER_NAME=" + config.server_names.at(0));
 	envVariables.push_back("SERVER_PORT=" + std::to_string(config.listen_port));
 	envVariables.push_back("PATH_INFO=" + pathInfo);
+	envVariables.push_back("REDIRECT_STATUS=200"); //for php-cgi?
 
 	if (method == "GET")
 		envVariables.push_back("QUERY_STRING=" + queryString);
@@ -132,7 +133,6 @@ static void parseCgiStatus(std::string status)
 
 static std::string parseCgiContentType(std::string contentType)
 {
-	//Content-Type: type "/" subtype *( OWS ";" OWS parameter )?? the ;param?? for content-length?
 	int valueTime = 0;
 	std::string finalValue;
 	
@@ -147,6 +147,8 @@ static std::string parseCgiContentType(std::string contentType)
 		}
 		else if (valueTime == 1 && (isalpha(contentType[i]) || contentType[i] == '/'))
 			finalValue += contentType[i];
+		else if (contentType[i] == ';')
+				break ; //Content-type: text/html;charset=UTF-8
 		else 
 			throw ErrorResponseException(500);
 
@@ -249,12 +251,11 @@ static std::pair<std::string, int> parseCgiHeaders(std::string cgiHeaders, size_
 
 void HttpRequest::parseCgiOutput(std::string cgiOutput)
 {
-	//allow /n/n instead of /r/n? same for the ones in between headers
 	std::string cgiBody;
 	std::string cgiHeaders;
 	std::pair<std::string, int> headerResult;
 
-	size_t pos = cgiOutput.find("\r\n\r\n"); //try out ex body first like body, headers or body headers body
+	size_t pos = cgiOutput.find("\r\n\r\n");
 	size_t findLen = 4;
 
 	if (pos == std::string::npos) 
@@ -277,11 +278,7 @@ void HttpRequest::parseCgiOutput(std::string cgiOutput)
 		cgiBody.resize(headerResult.second);
 	}
 	
-	std::cout << "CGI PARSING IS DONE\nSTATUS: 200" << "\"\nCONTENT TYPE VALUE: \"" << headerResult.first << "\"" << "CONTENT LENGTH: \""
-	<< headerResult.second << "\"" << std::endl;
-	std::cout << "CGI RESPONSE BODY: " << cgiBody << std::endl;
 	checkContentType(headerResult.first);
-	
 	httpResponse.setResponseHeader("content-type", headerResult.first);
 	httpResponse.setResponseBody(cgiBody);
 	httpResponse.setStatus(200);
@@ -327,7 +324,7 @@ void HttpRequest::doCgi(ServerConfig config, std::string cgiExtension, const Ser
 	close (stdinWriteFd);
 	int stdinFd;
 	int stdoutFd;
-	stdinFd = open("tempStdin", O_RDONLY);
+	stdinFd = open("tempStdin", O_RDONLY); //test? with post
 	if (stdinFd == -1)
 		throw ErrorResponseException(500);
 	stdoutFd = open("tempStdout", O_WRONLY | O_CREAT | O_TRUNC, 0666);
@@ -336,6 +333,7 @@ void HttpRequest::doCgi(ServerConfig config, std::string cgiExtension, const Ser
 		close(stdinFd);
 		throw ErrorResponseException(500);
 	}
+	//int stderrDebug = open("tempStderrDEBUG", O_WRONLY | O_CREAT | O_TRUNC, 0666);
 	pid_t pid = fork();
 	if (pid == -1)
 		throw ErrorResponseException(500);
@@ -346,6 +344,8 @@ void HttpRequest::doCgi(ServerConfig config, std::string cgiExtension, const Ser
 		(void)server;
 		try
 		{
+			//dup2(stderrDebug, STDERR_FILENO);
+			//close(stderrDebug);
 			if (dup2(stdinFd, STDIN_FILENO) == -1)
 			{
 				close(stdinFd);
@@ -378,7 +378,7 @@ void HttpRequest::doCgi(ServerConfig config, std::string cgiExtension, const Ser
 	{
 		close(stdinFd);
 		close(stdoutFd);
-		int status;
+		int status = 0;
 		const int TIMEOUT_SECONDS = 10;
 		std::time_t start_time = std::time(nullptr);
 		while (true)
@@ -400,6 +400,15 @@ void HttpRequest::doCgi(ServerConfig config, std::string cgiExtension, const Ser
 			usleep(500000);
 		}
 		std::cout << "CHILD STATUS: " << status << std::endl;
+		//test begin
+		// int stdErrdebugOpen = open("tempStderrDEBUG", O_RDONLY);
+		// std::cout << "DEBUG CGI ERROR OUTPUT" << std::endl;
+		// int readDebug;
+		// char buf[1000];
+		// while ((readDebug = read(stdErrdebugOpen, buf, sizeof(buf))) > 0)
+		// 	std::cout << buf << std::endl;
+		// std::cout << "DEBUG DONE" << std::endl;
+		//test done
 		if (!WIFEXITED(status) || WEXITSTATUS(status) != 0)
 			throw ErrorResponseException(500);
 
@@ -418,6 +427,7 @@ void HttpRequest::doCgi(ServerConfig config, std::string cgiExtension, const Ser
 			throw ErrorResponseException(500);
 		}
 		close(stdoutReadFd);
+		std::cout << "time to parse cgi output" << std::endl;
 		parseCgiOutput(cgiOutput);
 	}
 }
